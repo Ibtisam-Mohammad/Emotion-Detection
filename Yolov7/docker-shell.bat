056e4c9bdb81e7c4855849fb07592a102a86762c
Hemani Shah <hemanishah00@gmail.com>
2022-12-01 21:40:13 +0530

    I am

--- Yolov7/docker-shell.bat
+++ Yolov7/docker-shell.bat
@@ -1,3 +1,3 @@
 docker network inspect app-network >/dev/null 2>&1 || docker network create app-network
-docker build -t yolo .
-docker run --rm -it -p 9000:9000 --network app-network --mount type=bind,source="%cd%",target=/app --name yolo yolo
+docker pull us-west2-docker.pkg.dev/mercurial-snow-363512/yolo-repo/yolo-image:tag1
+docker run --rm -it -p 9000:9000 --network app-network --mount type=bind,source="%cd%",target=/app --name yolo us-west2-docker.pkg.dev/mercurial-snow-363512/yolo-repo/yolo-image:tag1
